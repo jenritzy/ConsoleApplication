9135fa80c9f0c935b36fd91ee69263391959ce32
jerit6490 <jerit6490@2Q3H0Q2>
2022-09-15 13:28:34 -0400

    Add project files.

---
+++ Console Application/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.Title = "Conversion";
+Console.Write("Please enter a number: ");
+double num = Convert.ToDouble(Console.ReadLine());
+Console.Write("Now enter another number: ");
+double sum = num + Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Total = " + sum);
+Console.ReadKey();
